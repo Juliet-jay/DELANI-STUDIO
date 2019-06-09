dbec38fbab85d646362a5787c1d3fe72ada08272
kathure <juliekmuthee@gmail.com>
2019-06-09 20:49:30 +0300

    js

--- js/script.js
+++ js/script.js
@@ -16,12 +16,29 @@ $(".i") .click(function(){
 
 
 
-$(".button").click(function(event){
+
+    $(".button").click(function(event){
        
-       var user =document.getElementById('input').value;
-       alert(user + "Hi we've recieved your message,thank you for contacting us");
+       var user =document.getElementById('name').value;
+       alert("Hi", + user +  "we've recieved your message,thank you for contacting us");
 
-       });
+       const Mailchimp = require("57ad93865f611e18fd8f6f2aa352ff87-us20");
 
+       const mailchimp = new Mailchimp(myMailchimpAPI);
+       
+       mailchimp.post(`/lists/${myListId}/members`, {
+         email_address: 'juliekmuthee@gmail.com',
+         status: 'subscribed',
+         merge_fields: {
+           FNAME: 'julie254',
+           EMAIL: 'juliekmuthee@gmail.com',
+           COMPANY: 'xyz'
+         }
+       })
+       .then(res => console.log(res))
+       .catch(err => console.log(err))
+    });
+
+       
 
     })
