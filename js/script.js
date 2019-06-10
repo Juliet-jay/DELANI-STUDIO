5bfeea7f02020cc8494e36cdf1bc5734b0ec06f4
kathure <juliekmuthee@gmail.com>
2019-06-10 09:03:36 +0300

    changes on css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-$(".img") .click(function(){
+$(document).ready(function(){
+$(".img").click(function(){
     $(".par").toggle();
 });
 $(".imag") .click(function(){
@@ -8,37 +8,11 @@ $(".imag") .click(function(){
 $(".i") .click(function(){
     $(".pa").toggle();
 });
+$("#w4").mouseenter(function () {
+    $(".w4").show();
+});
 
-
-    $(".img").hover(function () {
-        $(this).find(".line").toggle();
-    })
-
-
-
-
-    $(".button").click(function(event){
-       
-       var user =document.getElementById('name').value;
-       alert("Hi", + user +  "we've recieved your message,thank you for contacting us");
-
-       const Mailchimp = require("57ad93865f611e18fd8f6f2aa352ff87-us20");
-
-       const mailchimp = new Mailchimp(myMailchimpAPI);
-       
-       mailchimp.post(`/lists/${myListId}/members`, {
-         email_address: 'juliekmuthee@gmail.com',
-         status: 'subscribed',
-         merge_fields: {
-           FNAME: 'julie254',
-           EMAIL: 'juliekmuthee@gmail.com',
-           COMPANY: 'xyz'
-         }
-       })
-       .then(res => console.log(res))
-       .catch(err => console.log(err))
-    });
-
-       
-
-    })
+$("#w4").mouseleave(function () {
+    $(".w4").hide();
+});
+});
